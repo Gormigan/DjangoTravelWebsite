df38997018e99dfea1c6d3ed10864b08ee868057
Gormigan <herman.luik@gmail.com>
2021-04-16 12:38:29 +0300

    Changed the secret key and hid it.

--- DjangoTravelWebsite/settings.py
+++ DjangoTravelWebsite/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d544-o21u5x0x2$)z0xad*2u0oe4i*8*6$54s(h)b0%w_b%126'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
